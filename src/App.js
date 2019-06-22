2aee90b27d57e26da2a335ef447fe8fb8beb91f2
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-06-22 22:17:44 +0530

    Add components to main page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Akhil from './components/Akhil'
+import Sample from './components/Sample'
+
 
 function App() {
   return (
     <div className="App">
     
      <Akhil />
+     <Sample />
     </div>
   );
 }
